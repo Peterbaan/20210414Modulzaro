1dcf4a2febfeeeee91c87a3728bf77c2bbdfba1a
VinczeP <vincze.peter.eger@gmail.com>
2021-04-14 18:06:57 +0200

    1. Feladat kész

---
+++ 20210414-Mozulzaro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210414_Mozulzaro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("balkezesek.csv");
+            List<Balkezesek> balkezesek = new List<Balkezesek>();
+            foreach (string sor in sorok.Skip(1))
+            {
+                balkezesek.Add(new Balkezesek());
+            }
+            int N = balkezesek.Count;
+            Console.WriteLine($"1. Feladat: {N} veresenyző van");
+
+            Console.ReadLine();
+        }
+    }
+}
