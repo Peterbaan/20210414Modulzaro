fe8fe5f734afb2ba8e55b57c52285d951e8ecb41
VinczeP <vincze.peter.eger@gmail.com>
2021-04-14 18:44:35 +0200

    Felületesen kidolgozva

--- 20210414-Mozulzaro/Balkezesek.cs
+++ 20210414-Mozulzaro/Balkezesek.cs
@@ -9,11 +9,22 @@ namespace _20210414_Mozulzaro
     class Balkezesek
     {
 
-        public string név;
-        public int első;
-        public int utolsó;
-        public int súly;
-        public int magasság;
+        public string név { get; set; }
+        public int első { get; set; }
+        public int utolsó { get; set; }
+        public int súly { get; set; }
+        public int magasság { get; set; }
+
+        public Balkezesek(string sor)
+        {
+            string[] s = sor.Split(';');
+            név = (s[0]);
+            első = int.Parse(s[1]);
+            utolsó = int.Parse(s[2]);
+            súly = int.Parse(s[3]);
+            magasság = int.Parse(s[4]);
+
+        }
 
         public static int Count { get; internal set; }
     }
--- 20210414-Mozulzaro/Program.cs
+++ 20210414-Mozulzaro/Program.cs
@@ -11,14 +11,48 @@ namespace _20210414_Mozulzaro
     {
         static void Main(string[] args)
         {
+            int i;
             string[] sorok = File.ReadAllLines("balkezesek.csv");
-            List<Balkezesek> balkezesek = new List<Balkezesek>();
+            List<Balkezesek> balkezes = new List<Balkezesek>();
             foreach (string sor in sorok.Skip(1))
             {
-                balkezesek.Add(new Balkezesek());
+                balkezes.Add(new Balkezesek());
             }
-            int N = balkezesek.Count;
-            Console.WriteLine($"1. Feladat: {N} veresenyző van");
+            int N = balkezes.Count;
+            Console.WriteLine($"1. Feladat: {N} veresenyző van");  //1. Feladat
+
+            int első = Balkezesek.első;
+            int[] Elso = első;
+            int Db1980Felett;
+            for (i = 0; i < N; i++) { if (Balkezesek.első == 1980) { Db1980Felett++; } }
+            Console.WriteLine($"2. Feladat: {Db1980Felett} veresenyző lépett 1980-ban pályára először");
+
+
+            Console.WriteLine("Adjon meg egy évszámot 1900 és 1999 között!");  //4. Feladat
+            int Bekeres = int.Parse(Console.ReadLine());
+            if (!(1900 <= Bekeres && Bekeres <= 1999))
+            { }
+            else 
+            { Console.WriteLine("Hibás adat, adja meg újra!"); }
+
+
+            int minIndex = 0;   //5.Feladat
+            for (i = 1; i < N; i++)
+            {
+                if (Elso[i] < Elso[minIndex]) { minIndex = i; }
+
+            }
+            i = 0;              //6.Feladat
+            while (i < N && !(Elso[i] % 2 == 0))
+            {
+                i++;
+            }
+            bool van = i < N;
+
+            int JohnDB = 0;         //7.Feladat
+            for (i = 0; i < N; i++) { if (Balkezesek.név[i] == "John") { JohnDB++; } }
+
+
 
             Console.ReadLine();
         }
